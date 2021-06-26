dd74154f33c5ed383a2b8db6ab012ea738db6126
Peter Phan <peter_phan05@yahoo.com>
2021-06-26 01:29:23 -0700

    Home Leftside

---
+++ src/components/Rightside.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Rightside = props => {
+    return <Container>Right Side</Container>;
+};
+
+export default Rightside;
+
+const Container = styled.div`
+    grid-area: Rightside;
+`;
